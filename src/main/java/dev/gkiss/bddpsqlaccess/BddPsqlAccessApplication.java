edb7d21bf0626470a48dc446c107a4c39cbbd4aa
gergely <kiss-gergely@outlook.com>
2020-05-31 13:45:08 +0100

    first commit

---
+++ src/main/java/dev/gkiss/bddpsqlaccess/BddPsqlAccessApplication.java
@@ -0,0 +1,13 @@
+package dev.gkiss.bddpsqlaccess;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BddPsqlAccessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BddPsqlAccessApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/gkiss/bddpsqlaccess/BddPsqlAccessApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.gkiss.bddpsqlaccess;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BddPsqlAccessApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
